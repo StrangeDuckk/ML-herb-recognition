668e6e947e520844e4d73869beee4d3be48db03b
StrangeDuckk <mikusek.kasia@gmail.com>
2026-05-20 19:58:20 +0200

    added Fluent API for first table

--- HerbRecognition_APIs/Models/HerbRecognitionDbContext.cs
+++ HerbRecognition_APIs/Models/HerbRecognitionDbContext.cs
@@ -65,20 +65,24 @@ public partial class HerbRecognitionDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=herb-recognition-db;Username=postgres;Password=postgreSQL0323");
+        => optionsBuilder.UseNpgsql("DefaultConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Activesubstance>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("activesubstance_pk");
+            entity.HasKey(e => e.Id)
+                .HasName("activesubstance_pk");
 
             entity.ToTable("activesubstance");
 
             entity.Property(e => e.Id)
                 .UseIdentityAlwaysColumn()
                 .HasColumnName("id");
-            entity.Property(e => e.Activesubstance1).HasColumnName("activesubstance");
+
+            entity.Property(e => e.Activesubstance1)
+                .IsRequired()
+                .HasColumnName("activesubstance");
         });
 
         modelBuilder.Entity<Color>(entity =>
